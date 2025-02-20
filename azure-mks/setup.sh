baf9b3a6886c590be89331c61e60cb1b3248a73d
Aaron Phethean <aphethean@matatika.com>
2025-02-20 18:37:01 +0000

    Made the default size 2 for the nodepool1 (for tasks)

--- azure-mks/setup.sh
+++ azure-mks/setup.sh
@@ -1,7 +1,7 @@
 
 #export BUILD_AZ_RG=SERVICES
-#export BUILD_AZ_LOCATION=eastus
-#export BUILD_AZ_AKS_NAME=GREENAKS
+#export BUILD_AZ_LOCATION=ukwest
+#export BUILD_AZ_AKS_NAME=BLUEAKS
 
 [[ -z "$BUILD_AZ_RG" ]] && { echo "Error: BUILD_AZ_RG not found in env"; exit 1; }
 [[ -z "$BUILD_AZ_LOCATION" ]] && { echo "Error: BUILD_AZ_LOCATION not found in env"; exit 1; }
@@ -38,7 +38,7 @@ az aks get-credentials -n $BUILD_AZ_AKS_NAME -g $BUILD_AZ_RG
 #
 az aks nodepool add --name esystempool --cluster-name $BUILD_AZ_AKS_NAME --resource-group $BUILD_AZ_RG -s Standard_DS3_v2 --node-osdisk-type Ephemeral --node-count 1 --node-taints CriticalAddonsOnly=true:NoSchedule --mode System
 az aks nodepool delete --name nodepool1 --cluster-name $BUILD_AZ_AKS_NAME --resource-group $BUILD_AZ_RG
-az aks nodepool add --name nodepool1 --cluster-name $BUILD_AZ_AKS_NAME --resource-group $BUILD_AZ_RG -s Standard_DS3_v2 --node-osdisk-type Ephemeral --node-count 1 --min-count 1 --max-count 2 --enable-cluster-autoscaler
+az aks nodepool add --name nodepool1 --cluster-name $BUILD_AZ_AKS_NAME --resource-group $BUILD_AZ_RG -s Standard_DS3_v2 --node-osdisk-type Ephemeral --node-count 2 --min-count 2 --max-count 3 --enable-cluster-autoscaler
 az aks nodepool add --name appspool --cluster-name $BUILD_AZ_AKS_NAME --resource-group $BUILD_AZ_RG -s Standard_DS3_v2 --node-osdisk-type Ephemeral --node-count 1 --min-count 1 --max-count 3 --enable-cluster-autoscaler
 az aks nodepool add --name stagingapps --cluster-name $BUILD_AZ_AKS_NAME --resource-group $BUILD_AZ_RG -s Standard_DS3_v2 --node-osdisk-type Ephemeral --node-count 1 --min-count 1 --max-count 1 --enable-cluster-autoscaler
 
