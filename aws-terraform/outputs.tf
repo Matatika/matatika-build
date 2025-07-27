48906ff3d7dc921cbd9bd63f229643e6a369bf7a
Aaron Phethean <aphethean@matatika.com>
2025-07-27 16:30:51 +0100

    Output the rds address for use with the catalog and warehouse

--- aws-terraform/outputs.tf
+++ aws-terraform/outputs.tf
@@ -1,3 +1,8 @@
+output "catalog_rds_address" {
+  description = "The catalog and warehouse rds endpoint"
+  value       = module.db.db_instance_address
+}
+
 output "nat_gateway_eip" {
   description = "The static egress IP for outbound traffic"
   value       = module.vpc.nat_public_ips
